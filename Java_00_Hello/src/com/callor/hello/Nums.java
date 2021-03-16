e4b32560764ecb6c7618cc10a9c53a5ab8ad9a14
hooninfinity <hooninfinity@naver.com>
2021-03-16 14:06:59 +0900

    처음

---
+++ Java_00_Hello/src/com/callor/hello/Nums.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+public class Nums {
+	
+	public static void main(String[] args) {
+		System.out.println(3);
+		System.out.println("과");
+		System.out.println(5);
+		System.out.println("을 더하면");
+		System.out.println(3 + 5);
+	}
+
+}
---
+++ Names.java
@@ -0,0 +1,22 @@
+// 여기서부터 자바 코드가 시작된다
+// comment, 주석
+
+// Names 클래스 라고 읽는다
+// Names 클래스는 Names.java 라는 이름으로 파일저장을 해야함
+public class Names {
+
+	// main() { }, main()
+	// "main 메서드" 라고 읽는다
+	public static void main(String[] ar) {
+
+		// "" : 문자열
+		// ""(DQ)로 묶여 있는 내용을 보여달라
+		System.out.println("I am Kim");
+		System.out.println(30 + 40);
+		System.out.println(30 * 40);
+		System.out.println("30 + 40");
+		System.out.println(30 * 40 + 100);
+
+	}
+
+}

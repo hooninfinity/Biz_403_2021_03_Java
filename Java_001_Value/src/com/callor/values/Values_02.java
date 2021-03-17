cf8147c18bca348c48abf4752a1a26ed01ddee2b
hooninfinity <hooninfinity@naver.com>
2021-03-17 11:50:38 +0900

    Values

---
+++ Java_001_Value/src/com/callor/values/Values_02.java
@@ -0,0 +1,16 @@
+package com.callor.values;
+
+public class Values_02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1.반지름이 23인 원의 둘레와 넓이를 계산하여 출력");
+		System.out.println("============================");
+		System.out.print("둘레 : ");
+		System.out.println(23 * 2 * 3.14);
+		System.out.println("============================");
+		System.out.print("넓이 : ");
+		System.out.println(23 * 23 * 3.14);
+		
+	}
+}

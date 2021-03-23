c7c1af6d208de71d754caf5dc306348992238628
hoonfinity <hooninfinity@naver.com>
2021-03-23 21:14:18 +0900

    apps

--- Java_005_Application/src/com/callor/apps/Pay_01.java
+++ Java_005_Application/src/com/callor/apps/Pay_01.java
@@ -7,55 +7,77 @@ public class Pay_01 {
 		int nPay = 3_723_560; // 금액
 		int nPaper = 50_000; // 최고액면가 화폐
 		
-		// 정수 = 정수 / 정수  : 소수점 이하가 잘린다
+		// 정수 = 정수 / 정수 : 소수점이하가 잘린다
 		// 몫을 구하여 액면가액 만큼 개수를 계산
-		int nCount = nPay / nPaper;
-		System.out.println(nPaper + "원권 : " + nCount);
-		
-		// 액면가액만큼 개수를 구했으면 
+		int nCount = nPay / nPaper;		// 총금액/5만원의 몫을 nCount에 저장
+		System.out.println(nPaper +"원권 : " + nCount);
+		// 액면가액만큼 개수를 구했으면
 		// nPay에서 액면가액 금액만큼을 빼고 이후에 계산
 		// 3723560 % 50000 => 50000 * 매수 만큼을
-		// 		3723560 에서 뺀 값이 된다.
-		// nPay = nPay - (nPaper * nCount)
+		//			3723560에서 뺀 값이 된다.
+		// nPay = nPay - ( nPaper * nCount )
 		// nPay = nPay % nPaper;
-		nPay %= nPaper; 
-		nPaper /= 5;
+		nPay %= nPaper; // 총액 3723560을 5만원으로 나눈후 그 나머지값을 총액으로 정함
+		nPaper /= 5;	// nPaper를 5로 나누고 그값인 1만원이 nPaper가 됨
+		
+		nCount = nPay / nPaper; // 원권 수  = 총액(나머지값) 23560원 / 10000;
+		System.out.println(nPaper +"원권 : " + nCount);
+		nPay %= nPaper; // 총액은 23560원/10000의 나머지 3560을 총액으로 함
+		nPaper /= 2; // nPaper는 1만원을 2로 나눈 값인 5천원이 된다
 		
 		nCount = nPay / nPaper;
-		System.out.println(nPaper + "원권 : " + nCount);
-		nPay %= nPaper;
-		nPaper /= 2;
-		
-		nCount = nPay / nPaper;
-		System.out.println(nPaper + "원권 : " + nCount);
-		nPay %= nPaper;
-		nPaper /= 5;
-		
-		nCount = nPay / nPaper;
-		System.out.println(nPaper + "원권 : " + nCount);
-		nPay %= nPaper;
-		nPaper /= 2;
-		
-		nCount = nPay / nPaper;
-		System.out.println(nPaper + "원권 : " + nCount);
-		nPay %= nPaper;
-		nPaper /= 5;
-		
-		nCount = nPay / nPaper;
-		System.out.println(nPaper + "원권 : " + nCount);
-		nPay %= nPaper;
-		nPaper /= 2;
-		
-		nCount = nPay / nPaper;
-		System.out.println(nPaper + "원권 : " + nCount);
+		System.out.println(nPaper +"원권 : " + nCount);
 		nPay %= nPaper;
 		nPaper /= 5;
 
 		nCount = nPay / nPaper;
-		System.out.println(nPaper + "원권 : " + nCount);
+		System.out.println(nPaper +"원권 : " + nCount);
 		nPay %= nPaper;
 		nPaper /= 2;
+		
+		nCount = nPay / nPaper;
+		System.out.println(nPaper +"원권 : " + nCount);
+		nPay %= nPaper;
+		nPaper /= 5;
+
+		nCount = nPay / nPaper;
+		System.out.println(nPaper +"원권 : " + nCount);
+		nPay %= nPaper;
+		nPaper /= 2;
+		
+		nCount = nPay / nPaper;
+		System.out.println(nPaper +"원권 : " + nCount);
+		nPay %= nPaper;
+		nPaper /= 5;
+		
+		nCount = nPay / nPaper;
+		System.out.println(nPaper +"원권 : " + nCount);
+		nPay %= nPaper;
+		nPaper /= 2;
+
+		nCount = nPay / nPaper;
+		System.out.println(nPaper +"원권 : " + nCount);
+		nPay %= nPaper;
+		nPaper /= 5;
+		
+		nCount = nPay / nPaper;
+		System.out.println(nPaper +"원권 : " + nCount);
+		nPay %= nPaper;
+		nPaper /= 2;
+		
+		
+
+
+
+
+
+
+		
+		
+		
+		
+		
 		
 	}
-
+	
 }

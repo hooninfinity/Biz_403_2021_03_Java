38546ecbfb0f02fd00130efc8b1bd38075ba71a5
hoonfinity <hooninfinity@naver.com>
2021-03-25 21:04:00 +0900

    Service 복습

--- Java_007_Application/src/com/callor/apps/service/ScoreServiceV1.java
+++ Java_007_Application/src/com/callor/apps/service/ScoreServiceV1.java
@@ -3,32 +3,34 @@ package com.callor.apps.service;
 import java.util.Random;
 
 public class ScoreServiceV1 {
-	
-	int[] intKor = new int[5];
+	// 선언부
+	int[] intKor = new int[5]; // intKor; 로 끝나도 상관없음... 설명때문에 추가하신듯
 	int[] intEng;
 	int[] intMath;
-	
 	int[] intTotal;
 	float[] floatAvg;
-	// 생성자
+	
+	// 생성자(클래스 영역의 변수들을 초기화, 선언은 위에서 생성은 여기서)
 	public ScoreServiceV1() {
-		intKor = new int[10];
+		intKor = new int[10]; // 
 		intEng= new int[ intKor.length ]; // intKor배열의 갯수만큼 수가 정해짐. intKor배열의 숫자만 바꾸면 아래도 다 적용되도록 intKor.length 사용
 		intMath = new int[ intKor.length ];
 		
 		intTotal = new int[ intKor.length ];
 		floatAvg = new float[ intKor.length ];
 	}
+	
 	// 점수 만들기
 	public void makeScore() {
-		Random rnd = new Random();
+		Random rnd = new Random(); // 점수 만들때만 랜덤 사용하므로 여기서 생성
 		for(int i = 0 ; i < intKor.length; i++) {
-			intKor[i] = rnd.nextInt(100) + 1;
+			intKor[i] = rnd.nextInt(100) + 1; // Random 클래스에서 제공하는 nextInt() 명령을 수행하고 결과를 i에 저장
 			intMath[i] = rnd.nextInt(100) + 1;
 			intEng[i] = rnd.nextInt(100) + 1;
 		}
 	}
-	// 점수의 합계 만들기
+	
+	// 점수의 합계, 평균 만들기
 	public void makeSum() {
 		for(int i = 0 ; i < intKor.length ; i++) {
 			intTotal[i] = intKor[i];
@@ -37,6 +39,7 @@ public class ScoreServiceV1 {
 			floatAvg[i] = (float)intTotal[i] / 3;
 		}
 	}
+	
 	// 최종 출력하기
 	public void printScore() {
 		System.out.println("============================");
